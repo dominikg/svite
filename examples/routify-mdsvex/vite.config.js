755d76063dfebbd27cffcfd317b560100f364e12
dominikg <dominik.goepel@gmx.de>
2020-07-14 22:07:43 +0200

    docs: update routify example, declare routify as dependency instead of devDependency

--- examples/routify-mdsvex/vite.config.js
+++ examples/routify-mdsvex/vite.config.js
@@ -3,5 +3,6 @@ module.exports = {
   plugins: [svite()],
   optimizeDeps: {
     include: ['@sveltech/routify/runtime/plugins/tree', '@sveltech/routify/runtime/plugins/assignAPI', '@sveltech/routify/runtime'],
+    exclude: ['@sveltech/routify'],
   },
 };
