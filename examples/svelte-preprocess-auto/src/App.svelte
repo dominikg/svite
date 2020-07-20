16c25ec81b9f523edd567054cc012e84d8f89d34
dominikg <dominik.goepel@gmx.de>
2020-07-20 18:47:35 +0200

    fix: invalid formatting in nested stylus and pug

--- examples/svelte-preprocess-auto/src/App.svelte
+++ examples/svelte-preprocess-auto/src/App.svelte
@@ -6,9 +6,18 @@
 </script>
 
 <style type="text/stylus">
-  .note color #ff3e00 h1 color blue;
+  .note
+    color #ff3e00
+  h1
+    color blue
 </style>
 
 <template type="text/pug">
-  h1.note This is the svelte-preproces example - built with svite hr h1 Hello! br input(bind:value="{label}") br Nested("{label}")
+  h1.note This is the svelte-preproces example - built with svite
+  hr
+  h1 Hello!
+  br
+  input(bind:value="{label}")
+  br
+  Nested("{label}")
 </template>
