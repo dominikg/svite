55b89db7f64f28f5a76b7290e0ae22d24ef4f5a4
dominikg <dominik.goepel@gmx.de>
2020-07-23 23:44:38 +0200

    chore: update routify-mdsvex example to latest routify

--- examples/routify-mdsvex/vite.config.js
+++ examples/routify-mdsvex/vite.config.js
@@ -1,8 +1,5 @@
-const svite = require('svite');
 module.exports = {
-  plugins: [svite()],
   optimizeDeps: {
-    include: ['@sveltech/routify/runtime/plugins/tree', '@sveltech/routify/runtime/plugins/assignAPI', '@sveltech/routify/runtime'],
     exclude: ['@sveltech/routify'],
   },
 };
