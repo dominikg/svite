2d88b734872a6b273ca7dce4ceeaef57ccec4884
dominikg <dominik.goepel@gmx.de>
2020-08-23 01:52:06 +0200

    test: remove deprecated option from jest config

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,4 @@ module.exports = {
   testPathIgnorePatterns: ['/test/app/.*', '/test/temp/.*', '/examples/.*', '/node_modules/.*'],
   watchPathIgnorePatterns: ['<rootDir>/test/temp'],
   reporters: ['default', ['jest-junit', { outputDirectory: './test/temp' }]],
-  testResultsProcessor: 'jest-junit',
 };
