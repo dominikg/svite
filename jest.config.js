c0e9ceed27283bb7b4c7113c53bd9eae5517d6e5
dominikg <dominik.goepel@gmx.de>
2020-08-23 00:56:08 +0200

    test: add jest-junit

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,6 @@
 module.exports = {
   testPathIgnorePatterns: ['/test/app/.*', '/test/temp/.*', '/examples/.*', '/node_modules/.*'],
   watchPathIgnorePatterns: ['<rootDir>/test/temp'],
+  reporters: ['default', ['jest-junit', { outputDirectory: './test/temp' }]],
+  testResultsProcessor: 'jest-junit',
 };
