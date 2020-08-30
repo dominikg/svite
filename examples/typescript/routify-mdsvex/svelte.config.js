d413141523ca458c7ee9f6a3aae9e31fb8b32a68
dominikg <dominik.goepel@gmx.de>
2020-08-30 22:26:42 +0200

    feat: update routify-mdsvex example with an mdsvex layout

--- examples/typescript/routify-mdsvex/svelte.config.js
+++ examples/typescript/routify-mdsvex/svelte.config.js
@@ -1,6 +1,12 @@
+const path = require('path');
 const { mdsvex } = require('mdsvex');
 const { typescript } = require('svelte-preprocess');
 module.exports = {
   extensions: ['.svelte', '.svx'],
-  preprocess: [typescript(), mdsvex()],
+  preprocess: [
+    typescript(),
+    mdsvex({
+      layout: path.join(__dirname, 'src', 'layouts', 'MdsvexLayout.svelte'),
+    }),
+  ],
 };
